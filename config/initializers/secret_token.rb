7c0e88fa326060fe0030f05844bebd40f06b92e2
Jarl Friis <jarl@softace.dk>
2012-09-08 21:06:53 +0200

    Initial files from 'rails new multiplefileuplad -J -T -O -S'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multiplefileuplad::Application.config.secret_token = '09678d937d81a2bf39bf2a563f15a8b0d820d44168b73886e56488e70dc4c6218e4f73179124a2e9385152e87799b4132376d7c981e9e3a14f4bf4c791cef741'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Multiplefileuplad::Application.config.session_store :cookie_store, key: '_multiplefileuplad_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Multiplefileuplad::Application.config.session_store :active_record_store
