a1e426ecceb632744cc1f88638639d046aa4cf84
Jarl Friis <jarl@softace.dk>
2012-09-12 15:58:38 +0200

    Refactored test to use example_groups and added test for POST

--- spec/controllers/uploads_controller_spec.rb
+++ spec/controllers/uploads_controller_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
+shared_examples_for "any request" do
+  it "returns http success and" do
+    subject
+    expect(response).to render_template('new')
+    response.should be_success
+  end
+end
+
+shared_examples_for "a request made by an unknown user agent" do |args|
+  it_behaves_like "any request"
+  it "creates a new record for the user agent" do
+    expect { subject }.
+      to change{ UserAgent.exists?(:user_agent_string => test_user_agent) }.
+      from(false).to(true)
+  end
+  it "sets counter for #{args[:request_counter]} to == 1" do
+    subject
+    UserAgent.find_by_user_agent_string(test_user_agent).send(:"#{args[:request_counter]}_count").should == 1
+  end
+end
+
+shared_examples_for "a request made by a known user agent" do |args|
+  it_behaves_like "any request"
+  it "increases the counter for #{args[:request_counter]}" do
+    expect { subject }.
+      to change{ UserAgent.find_by_user_agent_string(test_user_agent).send(:"#{args[:request_counter]}_count") }.
+      by(1)
+  end
+end
+
 describe UploadsController do
   let(:test_user_agent) { 'Rails Testing' }
   context "For a known user agent" do
@@ -9,99 +39,52 @@ describe UploadsController do
     end
     
     describe "GET 'new'" do
-      before(:each) { get 'new_get'}
+      subject { get 'new_get' }
+      it_behaves_like "a request made by a known user agent", {:request_counter => 'get'}
+    end
 
-      it "returns http success and" do
-        expect(response).to render_template('new')
-        response.should be_success
-      end
+    describe "POST 'new' without files" do
+      subject { post 'new_post', :upload => {:files => [] } }
+      it_behaves_like "a request made by a known user agent", {:request_counter => 'post_zero'}
+    end
 
-      it "increases number of user_agent records" do
-        expect { get 'new_get' }.to change{ UserAgent.find_by_user_agent_string(test_user_agent).get_count }.from(1).to(2)
-      end
+    describe "POST 'new' with a single file" do
+      let(:test_file) { fixture_file_upload('/files/oneliner.txt', 'text/plain') }
+      subject { post 'new_post', :upload => {:files => [test_file] } }
+      it_behaves_like "a request made by a known user agent", {:request_counter => 'post_single'}
+    end
+
+    describe "POST 'new' with two files" do
+      let(:test_file) { fixture_file_upload('/files/oneliner.txt', 'text/plain') }
+      subject { post 'new_post', :upload => {:files => [test_file, test_file] } }
+      it_behaves_like "a request made by a known user agent", {:request_counter => 'post_multiple'}
     end
 
   end
 
   context "For an unknown user agent" do
-    before(:all) do
-      UserAgent.delete_all
-    end
+    before(:all) { UserAgent.delete_all }
 
     describe "GET 'new'" do
-      it "returns http success and" do
-        get 'new_get'
-        expect(response).to render_template('new')
-        response.should be_success
-      end
-      
-      it "creates a new record for an unknown user agent" do
-        expect { get 'new_get' }.to change{ UserAgent.exists?(:user_agent_string => test_user_agent) }.from(false).to(true)
-      end
-
-      it "sets new records to have get_count = 1" do
-        get 'new_get'
-        UserAgent.find_by_user_agent_string(test_user_agent).get_count.should == 1
-      end
+      subject { get 'new_get' }
+      it_behaves_like "a request made by an unknown user agent", {:request_counter => 'get'}
     end
 
     describe "POST 'new' without files" do
-      let(:invoke_post_new) { post 'new_post', :upload => {:files => [] } }
-      
-      it "returns http success and" do
-        invoke_post_new
-        expect(response).to render_template('new')
-        response.should be_success
-      end
-      
-      it "creates a new record for an unknown user agent" do
-        expect { invoke_post_new }.to change{ UserAgent.exists?(:user_agent_string => test_user_agent) }.from(false).to(true)
-      end
-
-      it "sets new records to have post_zero_count = 1" do
-        invoke_post_new
-        UserAgent.find_by_user_agent_string(test_user_agent).post_zero_count.should == 1
-      end
+      subject { post 'new_post', :upload => {:files => [] } }
+      it_behaves_like "a request made by an unknown user agent", {:request_counter => 'post_zero'}
     end
 
     describe "POST 'new' with a single file" do
       let(:test_file) { fixture_file_upload('/files/oneliner.txt', 'text/plain') }
-      let(:invoke_post_new) { post 'new_post', :upload => {:files => [test_file] } }
-      
-      it "returns http success and" do
-        invoke_post_new
-        expect(response).to render_template('new')
-        response.should be_success
-      end
-      
-      it "creates a new record for an unknown user agent" do
-        expect { invoke_post_new }.to change{ UserAgent.exists?(:user_agent_string => test_user_agent) }.from(false).to(true)
-      end
-
-      it "sets new records to have post_single_count = 1" do
-        invoke_post_new
-        UserAgent.find_by_user_agent_string(test_user_agent).post_single_count.should == 1
-      end
+      subject { post 'new_post', :upload => {:files => [test_file] } }
+      it_behaves_like "a request made by an unknown user agent", {:request_counter => 'post_single'}
     end
 
     describe "POST 'new' with two files" do
       let(:test_file) { fixture_file_upload('/files/oneliner.txt', 'text/plain') }
-      let(:invoke_post_new) { post 'new_post', :upload => {:files => [test_file, test_file] } }
-      
-      it "returns http success and" do
-        invoke_post_new
-        expect(response).to render_template('new')
-        response.should be_success
-      end
-      
-      it "creates a new record for an unknown user agent" do
-        expect { invoke_post_new }.to change{ UserAgent.exists?(:user_agent_string => test_user_agent) }.from(false).to(true)
-      end
-
-      it "sets new records to have post_multiple_count = 1" do
-        invoke_post_new
-        UserAgent.find_by_user_agent_string(test_user_agent).post_multiple_count.should == 1
-      end
+      subject { post 'new_post', :upload => {:files => [test_file, test_file] } }
+      it_behaves_like "a request made by an unknown user agent", {:request_counter => 'post_multiple'}
     end
   end
 end
